15a20f48e8e0978ec582d3457124728f74749e17
VivekKumarRay22 <vivekkumarray855@gmail.com>
2026-07-26 17:32:21 +0530

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-const app = require("./src/app")
+require("dotenv").config()
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
 
-app.listen(3000,()=>{
+connectToDB()
+
+app.listen(3000, () => {
     console.log('server running on port 3000');
-    
+
 })
