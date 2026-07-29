d65cae548f6471e9a3a7bbfe844981adf4d1eaf7
VivekKumarRay22 <vivekkumarray855@gmail.com>
2026-07-29 15:17:21 +0530

    feat: register api created - authRouter created - check if user exists already - hash the password - create user and sign token - set cookie by installing cookie-parser - user created/registered1'

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const userModel = require("../models/user.model")
+const authRouter = express.Router()
+const crypto = require("crypto")
+const jwt = require("jsonwebtoken")
+
+
+
+/**
+ * * @routes POST /api/auth/register
+ */
+authRouter.post("/register", async (req, res) => {
+    const { username, email, password, bio, profileImage } = req.body
+
+    const isUserAlreadyExists = userModel.findOne({
+        $or: [{ username: username }, { email: email }]
+    })
+
+    if (isUserAlreadyExists) {
+        res.status(409).json({
+            message: "user already exists with" + (username === isUserAlreadyExists.username ? "username" : "email")
+        })
+    }
+
+    const hash = crypto.createHash("sha256").update(password).digest("hex")
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash,
+        bio,
+        profileImage
+    })
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET, { expiresIn: "1d" })
+
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "user registered successfully",
+        user: {
+            email: user.email,
+            username: user.username,
+            bio: user.bio,
+            profileImage: user.profileImage
+        }
+    })
+
+})
+
+
+
+
+
+
+module.exports = authRouter
