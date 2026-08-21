02edf646ad0b030762cdb1e584dca2b56263f5c9
VivekKumarRay22 <vivekkumarray855@gmail.com>
2026-08-21 16:28:14 +0530

    feat: add getMe endpoint and authentication middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -85,7 +85,16 @@ async function loginUser(req, res) {
   })
 }
 
+async function getMe(req, res) {
+  const user = await userModel.findById(req.user.id)
+  res.status(200).json({
+    message: "user fetched successfully ",
+    user,
+  })
+}
+
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express")
 const router = Router()
 const authController = require("../controllers/auth.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
 
 router.post("/register", authController.registerUser)
 router.post("/login", authController.loginUser)
+router.get("/get-me", authMiddleware.authUser, authController.getMe)
 
 module.exports = router
